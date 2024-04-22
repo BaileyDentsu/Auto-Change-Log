33c7aac84cda21edf70773b2c7d8c38a510f3ac5
BaileyDentsu <167714201+BaileyDentsu@users.noreply.github.com>
2024-04-22 09:14:00 +0100

    Update streamlit_app.py
    Changed 'URL' input to 'address' input to match ScreamingFrog output

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 def compare_csv(df1, df2, columns_to_compare):
-    # Set the URL column as index for easy comparison
-    df1.set_index('URL', inplace=True)
-    df2.set_index('URL', inplace=True)
+    # Set the 'address' column as index for easy comparison
+    df1.set_index('address', inplace=True)
+    df2.set_index('address', inplace=True)
 
     # Identify added, removed, and modified URLs
     added = df2.index.difference(df1.index)
@@ -15,24 +15,24 @@ def compare_csv(df1, df2, columns_to_compare):
     data = []
 
     # Process added URLs
-    for url in added:
-        row = df2.loc[url]
+    for address in added:
+        row = df2.loc[address]
         data.append(
-            ['Added', url] + ['' for _ in columns_to_compare] + [row.get(col, '') for col in columns_to_compare]
+            ['Added', address] + ['' for _ in columns_to_compare] + [row.get(col, '') for col in columns_to_compare]
         )
 
     # Process removed URLs
-    for url in removed:
-        row = df1.loc[url]
+    for address in removed:
+        row = df1.loc[address]
         data.append(
-            ['Removed', url] + [row.get(col, '') for col in columns_to_compare] + ['' for _ in columns_to_compare]
+            ['Removed', address] + [row.get(col, '') for col in columns_to_compare] + ['' for _ in columns_to_compare]
         )
 
     # Process modified URLs
-    for url in common:
-        row1 = df1.loc[url]
-        row2 = df2.loc[url]
-        changes = ['Modified', url]
+    for address in common:
+        row1 = df1.loc[address]
+        row2 = df2.loc[address]
+        changes = ['Modified', address]
         modified = False  # Flag to track if any changes exist
 
         for col in columns_to_compare:
@@ -49,7 +49,7 @@ def compare_csv(df1, df2, columns_to_compare):
             data.append(changes)
 
     # Define the columns for the output DataFrame
-    output_columns = ['Added/Removed/Modified', 'URL']
+    output_columns = ['Added/Removed/Modified', 'address']
     for col in columns_to_compare:
         output_columns.extend([f'Old {col}', f'New {col}'])
 
@@ -67,7 +67,7 @@ if file1 and file2:
     df1 = pd.read_csv(file1)
     df2 = pd.read_csv(file2)
     columns = df1.columns.tolist()
-    columns.remove('URL')  # Assume 'URL' should not be chosen for comparison
+    columns.remove('address')  # Assume 'address' should not be chosen for comparison
     selected_columns = st.multiselect('Select columns to compare:', options=columns)
 
     if st.button('Compare Files'):
