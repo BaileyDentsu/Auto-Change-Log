48dcae6a88f6fee75df28cd3023162374881f9da
BaileyDentsu <167714201+BaileyDentsu@users.noreply.github.com>
2024-08-07 08:04:25 -0400

    fixed indentation issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,76 +2,74 @@ import streamlit as st
 import pandas as pd
 
 def compare_csv(df1, df2, columns_to_compare):
+    """
+    Compare two CSV files and identify added, removed, and modified entries.
+    
+    Args:
+    df1 (DataFrame): First dataframe to compare
+    df2 (DataFrame): Second dataframe to compare
+    columns_to_compare (list): List of column names to compare
+    
+    Returns:
+    DataFrame: Comparison results
+    """
     # Setting 'Address' as index for comparison
     df1.set_index('Address', inplace=True)
     df2.set_index('Address', inplace=True)
 
+    # Identify added, removed, and common addresses
     added = df2.index.difference(df1.index)
     removed = df1.index.difference(df2.index)
     common = df1.index.intersection(df2.index)
 
     data = []
 
-    # Added URLs
-    for Address in added:
-        row = df2.loc[Address]
-        # Append blank 'Old' values and current 'New' values for each column to compare
-        data.append(['Added', Address] + [item for col in columns_to_compare for item in ('', row.get(col, ''))])
+    # Process added URLs
+    for address in added:
+        row = df2.loc[address]
+        data.append(['Added', address] + [item for col in columns_to_compare for item in ('', row.get(col, ''))])
 
-    # Removed URLs
-    for Address in removed:
-        row = df1.loc[Address]
-        # Append current 'Old' values and blank 'New' values for each column to compare
-        data.append(['Removed', Address] + [item for col in columns_to_compare for item in (row.get(col, ''), '')])
+    # Process removed URLs
+    for address in removed:
+        row = df1.loc[address]
+        data.append(['Removed', address] + [item for col in columns_to_compare for item in (row.get(col, ''), '')])
 
-    # Modified URLs
-    for Address in common:
-        row1 = df1.loc[Address]
-        row2 = df2.loc[Address]
-        changes = ['Modified', Address]
+    # Process modified URLs
+    for address in common:
+        row1 = df1.loc[address]
+        row2 = df2.loc[address]
+        changes = ['Modified', address]
         row_entries = []
-        modified = False  # Reset flag for each URL
-        
+        modified = False
+
         for col in columns_to_compare:
             old_val = row1.get(col, '')
             new_val = row2.get(col, '')
-       
+
             # Convert to single values if Series
             old_val = old_val.iloc[0] if isinstance(old_val, pd.Series) and not old_val.empty else old_val
             new_val = new_val.iloc[0] if isinstance(new_val, pd.Series) and not new_val.empty else new_val
-       
-            row_entries.append(old_val)
-            if old_val != new_val:
-                row_entries.append(new_val)
-                modified = True
-            else:
-                row_entries.append('')  # Append blank for unchanged 'New' values
 
-"""
-OLD CODE CAUSING ERRORS
-        for col in columns_to_compare:
-            old_val = row1.get(col, '')
-            new_val = row2.get(col, '')
             row_entries.append(old_val)
             if old_val != new_val:
                 row_entries.append(new_val)
                 modified = True
             else:
                 row_entries.append('')  # Append blank for unchanged 'New' values
-"""
 
         if modified:
             changes.extend(row_entries)
             data.append(changes)
 
-    # Constructing output DataFrame headers
+    # Construct output DataFrame headers
     output_columns = ['Added/Removed/Modified', 'Address']
     for col in columns_to_compare:
         output_columns.extend([f'Old {col}', f'New {col}'])
 
-    # Creating DataFrame from the collected data
+    # Create DataFrame from the collected data
     return pd.DataFrame(data, columns=output_columns)
 
+# Streamlit UI
 st.title('CSV Comparison Tool')
 
 # File uploaders
@@ -79,17 +77,26 @@ file1 = st.file_uploader("Choose the first CSV file", type=['csv'])
 file2 = st.file_uploader("Choose the second CSV file", type=['csv'])
 
 if file1 and file2:
+    # Read CSV files into DataFrames
     df1 = pd.read_csv(file1)
     df2 = pd.read_csv(file2)
+    
+    # Get columns for comparison (excluding 'Address')
     columns = df1.columns.tolist()
-    columns.remove('Address')  # Assuming 'Address' should not be compared
+    columns.remove('Address')
+    
+    # Allow user to select columns for comparison
     selected_columns = st.multiselect('Select columns to compare:', options=columns)
 
     if st.button('Compare Files'):
         if selected_columns:
+            # Perform comparison
             result_df = compare_csv(df1, df2, selected_columns)
-            st.dataframe(result_df)
             
+            # Display results
+            st.dataframe(result_df)
+
+            # Provide download option for results
             csv = result_df.to_csv(index=False).encode('utf-8')
             st.download_button("Download output CSV", csv, "changeLog.csv", "text/csv", key='download-csv')
         else:
